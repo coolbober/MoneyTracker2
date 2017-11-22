c1293610ba5d751df71c8155baab72ee93873720
Max Zakharov <zakharovmaxim.92@gmail.com>
2017-11-23 01:00:07 +0300

    Homework №6. "Intent" and "textColor" didn't/

--- app/src/main/java/com/loftschool/moneytracker2/AddActivity.java
+++ app/src/main/java/com/loftschool/moneytracker2/AddActivity.java
@@ -1,34 +1,57 @@
 package com.loftschool.moneytracker2;
 
 import android.app.FragmentManager;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import static com.loftschool.moneytracker2.R.layout.activity_add;
+
 public class AddActivity extends AppCompatActivity {
+
+    public static final String EXTRA_TYPE = "type";
+    public static final String RESULT_ITEM = "item";
+    public static final int RC_ADD_ITEM = 99;
 
     private EditText name;
     private  EditText price;
     private ImageButton add;
+    private String type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(activity_add);
 
         getSupportFragmentManager();
         FragmentManager manager = getFragmentManager();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         name = findViewById(R.id.name);
         price = findViewById(R.id.number);
         add = findViewById(R.id.add);
         add.setEnabled(false);
+        type = getIntent().getStringExtra(EXTRA_TYPE);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent result = new Intent();
+                result.putExtra(RESULT_ITEM, new Item(name.getText().toString(), 100, type));
+                setResult(RESULT_OK, result);
+                finish();
+            }
+        });
 
         name.addTextChangedListener(new TextWatcher() {
             @Override
--- app/src/main/java/com/loftschool/moneytracker2/ItemsFragment.java
+++ app/src/main/java/com/loftschool/moneytracker2/ItemsFragment.java
@@ -1,7 +1,9 @@
 package com.loftschool.moneytracker2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
@@ -18,6 +20,7 @@ import com.loftschool.moneytracker2.api.Api;
 import java.io.IOException;
 import java.util.List;
 
+import static android.app.Activity.RESULT_OK;
 import static com.loftschool.moneytracker2.Item.TYPE_UNKNOWN;
 
 
@@ -68,7 +71,15 @@ public class ItemsFragment extends Fragment {
         RecyclerView recycler = view.findViewById(R.id.recycler);
         recycler.setLayoutManager(new LinearLayoutManager(getContext()));
         recycler.setAdapter(adapter);
-
+        FloatingActionButton fab = view.findViewById(R.id.fab_add);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AddActivity.class);
+                intent.putExtra(AddActivity.EXTRA_TYPE, type);
+                startActivityForResult(intent, AddActivity.RC_ADD_ITEM);
+            }
+        });
         loadItems();
     }
 
@@ -109,6 +120,15 @@ public class ItemsFragment extends Fragment {
 
     private void showError (String error) {
         Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode ==  AddActivity.RC_ADD_ITEM && resultCode == RESULT_OK) {
+            Item item = (Item) data.getSerializableExtra(AddActivity.RESULT_ITEM);
+            Toast.makeText(getContext(), item.name, Toast.LENGTH_LONG).show();
+        }
     }
 
     ///////////////////////////////////////////////////////////
